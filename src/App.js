7fbed21503473f751c950e32b1f2387fae7746a6
Seri <2364138keigo@gmail.com>
2026-03-08 23:31:09 +0900

    課題1をコピペ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("0");
+  const [records, setRecords] = useState([]);
+  const [error, setError] = useState("");
+  const [timeList, setTimeList] = useState([0]);
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleTime = (e) => {
+    setTime(e.target.value);
+  };
+  const handleClick = () => {
+    if (title == "" || time == 0 || time == "") {
+      setError("入力されていない項目があります");
+    } else {
+      setRecords([...records, { title: title, time: time }]);
+      setTimeList([...timeList, time]);
+      setTitle("");
+      setTime(0);
+      setError("");
+    }
+  };
+  console.log(timeList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>学習記録一覧</h1>
+      <p>学習内容</p>
+      <input value={title} onChange={handleTitle} />
+      <p>学習時間</p>
+      <input value={time} onChange={handleTime} />
+      <p>入力されている学習内容：{title}</p>
+      <p>入力されている時間：{time}時間</p>
+      {records.map((record) => {
+        return (
+          <p>
+            {record.title} {record.time}時間
+          </p>
+        );
+      })}
+      <button onClick={handleClick}>登録</button>
+      <p>
+        合計時間：
+        {timeList.reduce((accumlator, currentValue) => {
+          return parseInt(accumlator) + parseInt(currentValue);
+        })}
+        /1000(h)
+      </p>
+      <p>{error}</p>
+    </>
   );
 }
-
-export default App;
