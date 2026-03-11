fd7446e75c373e9a881373bd1166edde50863071
Seri <2364138keigo@gmail.com>
2026-03-11 11:36:04 +0900

    データを登録するための関数を追加

--- src/utils/supabaseFunctions.js
+++ src/utils/supabaseFunctions.js
@@ -4,3 +4,7 @@ export const getAllRecords = async () => {
   const records = await supabase.from("study-record").select("*");
   return records;
 };
+
+export const insertRecord = async (record) => {
+  await supabase.from("study-record").insert(record);
+};
